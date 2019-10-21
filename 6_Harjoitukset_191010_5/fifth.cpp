3a88f3a6b7635fbe5ec05117f72b056c3d18fd2d
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2019-10-21 13:15:31 +0300

    Added comments and other fixes

--- 6_Harjoitukset_191010_5/fifth.cpp
+++ 6_Harjoitukset_191010_5/fifth.cpp
@@ -1,4 +1,4 @@
-#define DEBUG_MODE true
+//#define DEBUG_MODE true
 #ifdef DEBUG_MODE
 #  define DEBUG(x) do {std::cerr << x << std::endl;} while (0)
 #else
@@ -16,36 +16,23 @@ using std::vector;
 
 // A workaround for threads only accepting one argument
 struct StartStop {
-	size_t start;
-	size_t stop;
-	unsigned int thread_num;
+	unsigned long long start;
+	unsigned long long stop;
+	unsigned long long thread_num;
 };
 
 
 long double count_pi_range(StartStop start_stop) {
 	// Calculates the pi sequence in a given range
-	long double result = 0.0;
-	size_t start=start_stop.start, stop=start_stop.stop;
 
-	size_t quarter = stop - start / 4;
-	size_t half    = stop - start / 2;
-	size_t three_quarters = ((stop - start) * 3) / 4;
+	long double result = 0.0;
+	unsigned long long start=start_stop.start, stop=start_stop.stop;
+
+	unsigned long long quarter = stop - start / 4;
+	unsigned long long half    = stop - start / 2;
+	unsigned long long three_quarters = ((stop - start) * 3) / 4;
 
 	for (start; start <= stop; ++start) {
-
-		if (DEBUG_MODE) {
-
-			if (start == quarter) {
-				DEBUG("Thread #" << start_stop.thread_num << ": 25% done");
-			}
-			else if (start == half) {
-				DEBUG("Thread #" << start_stop.thread_num << ": 50% done");
-			}
-			else if (start == three_quarters) {
-				DEBUG("Thread #" << start_stop.thread_num << ": 75% done");
-			}
-
-		}
 
 		if (start % 2 && start != 0) {
 			result += (4. / ((long double)start * 2. - 1.));
@@ -68,7 +55,7 @@ long double count_pi_multithreaded(size_t n) {
 
 	long double result = 0.0L;
 
-	unsigned long nthreads = std::thread::hardware_concurrency();
+	unsigned long long nthreads = std::thread::hardware_concurrency();
 	if (nthreads == 0UL) { // If failed to detect hardware threads, fall back to 2, as it's almost guaranteed to be available
 		nthreads = 2UL;
 	}
@@ -79,17 +66,17 @@ long double count_pi_multithreaded(size_t n) {
 	threadpool.reserve(nthreads);
 
 	// Populate the threadpool
-	for (unsigned int i = 0; i < nthreads; ++i) {
-		size_t start, stop;
+	for (unsigned long long i = 0; i < nthreads; ++i) {
+		unsigned long long start, stop;
 		start = (n / nthreads) * i + 1;
 		if (nthreads - 1 == i) {
 			stop = n;
 		}
 		else {
-			stop = (n / nthreads) * (i + 1U);
+			stop = (n / nthreads) * (i + 1ULL);
 		}
 		DEBUG("Thread #" << i + 1 << ": start: " << start << " stop: " << stop);
-		threadpool.push_back(std::async(count_pi_range, StartStop{ start, stop, i+1 }));
+		threadpool.push_back(std::async(count_pi_range, StartStop{ start, stop, i+1ULL }));
 	}
 
 	// Join the results of the threadpool
@@ -110,14 +97,16 @@ int main(void) {
 	const int BITS = 64;
 	if (test_num > test_num+1) {
 		const int BITS = 32;
-		DEBUG("Warning: using 32-bit values as the program was compiled in 32-bit mode");
+		DEBUG("Warning: accuracy of results may vary, as the program was compiled in 32-bit mode");
 	}
 
-	size_t terms;
+	unsigned long long terms;
 	std::cout << "Enter number of terms: ";
 	std::cin >> terms;
 
 	auto pi = count_pi_multithreaded(terms);
 
 	std::cout << "Pi: " << std::setprecision(BITS) << pi << std::endl;
+	
+	return EXIT_SUCCESS;
 }
