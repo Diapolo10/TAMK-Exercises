9d618c5d22e0667d7a6f767a098d8527f4477ac3
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2019-10-31 10:11:18 +0200

    Added assignment 8

---
+++ 8_Harjoitukset_191031_3/third.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std::string_literals;
+
+float virta(float jannite, float resistanssi) {
+	// U == R*I -> I == U/R
+	return jannite / resistanssi;
+}
+
+int main(void) {
+	float voltage{}, resistance{};
+
+	std::cout << u8"Virtapiirin jännite (V): ";
+	std::cin >> voltage;
+
+	std::cout << u8"Virtapiirin resistanssi (Ω): ";
+	std::cin >> resistance;
+
+	std::cout << u8"Virtapiirin sähkövirta on " << virta(voltage, resistance) << " A.\n";
+}
