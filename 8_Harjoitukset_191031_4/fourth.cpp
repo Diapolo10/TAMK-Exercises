acc08531547b2600dba8b6b9f4bd65625e611458
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2019-10-31 10:44:54 +0200

    Added 8.4

---
+++ 8_Harjoitukset_191031_4/fourth.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using std::string;
+using namespace std::string_literals;
+
+void sulake(double teho) {
+	double current = teho / 220;
+	string result {};
+
+	if ((current - 10) <= 0) {
+		result = "sulake on 10A";
+	}
+	else if ((current - 6) <= 10) {
+		result = "sulake on 16A";
+	}
+	else if ((current - 4) <= 16) {
+		result = "sulake on 20A";
+	}
+	else {
+		result = "Laitteen taakse tarvitaan useampi sulake";
+	}
+
+	std::cout << result << std::endl;
+}
+
+int main(void) {
+	double power;
+
+	std::cout << u8"Anna sähkölaitteen teho: ";
+	std::cin >> power;
+
+	sulake(power);
+}
