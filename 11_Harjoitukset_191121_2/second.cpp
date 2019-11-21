95ce15998c03638c928b4687e063063f43a15e04
Lari Liuhamo <Diapolo10@users.noreply.github.com>
2019-11-21 10:26:53 +0200

    Added the final set of exercises

---
+++ 11_Harjoitukset_191121_2/second.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+const int COUNTRY_COUNT{ 2 };
+
+struct Valtio {
+	string nimi;
+	long asukasluku;
+	double pinta_ala;
+	string johtaja;
+};
+
+int main(void) {
+	vector<Valtio> countries{};
+
+	while (countries.size() < COUNTRY_COUNT) {
+		string name{};
+		long population{};
+		double area{};
+		string leader{};
+		std::cout << "Valtio #" << countries.size() + 1 << ":\n";
+
+		std::cout << "Nimi: ";
+		std::cin >> name;
+
+		std::cout << "Asukasluku: ";
+		std::cin >> population;
+
+		std::cout << "Pinta-ala: ";
+		std::cin >> area;
+
+		std::cout << "Johtaja: ";
+		std::cin >> leader;
+
+		std::cout << std::endl;
+
+		countries.push_back(Valtio{ name, population, area, leader });
+	}
+
+	for (auto country : countries) {
+		std::cout <<
+			"Nimi: " << country.nimi << "\n" <<
+			"Asukasluku: " << country.asukasluku << "\n" <<
+			"Pinta-ala: " << country.pinta_ala << "\n" <<
+			"Johtaja: " << country.johtaja << "\n";
+		std::cout << std::endl;
+	}
+}
---
+++ 11_Harjoitukset_191121_3/third.cpp
@@ -0,0 +1,66 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+const int STUDENT_COUNT{ 5 };
+
+struct Opiskelija {
+	string etunimi;
+	string sukunimi;
+	string opiskelijanumero;
+	int opintopisteet;
+};
+
+void tulostaOpiskelijanTiedot(Opiskelija&);
+Opiskelija luoOpiskelija(void);
+
+int main(void) {
+	vector<Opiskelija> students{};
+
+	while (students.size() < STUDENT_COUNT) {
+
+		std::cout << "Opiskelija #" << students.size() + 1 << ":\n";
+		auto student = luoOpiskelija();
+
+		std::cout << std::endl;
+
+		students.push_back(student);
+	}
+
+	for (auto student : students) {
+		tulostaOpiskelijanTiedot(student);
+	}
+}
+
+void tulostaOpiskelijanTiedot(Opiskelija& student) {
+	std::cout <<
+		"Etunimi: " << student.etunimi << "\n" <<
+		"Sukunimi: " << student.sukunimi << "\n" <<
+		"Opiskelijanumero: " << student.opiskelijanumero << "\n" <<
+		"Opintopisteet: " << student.opintopisteet << "\n";
+	std::cout << std::endl;
+}
+
+Opiskelija luoOpiskelija(void) {
+	string first_name{};
+	string last_name{};
+	string student_number{};
+	int score{};
+
+	std::cout << "Etunimi: ";
+	std::cin >> first_name;
+
+	std::cout << "Sukunimi: ";
+	std::cin >> last_name;
+
+	std::cout << "Opiskelijanumero: ";
+	std::cin >> student_number;
+
+	std::cout << "Opintopisteet: ";
+	std::cin >> score;
+
+	return Opiskelija{ first_name, last_name, student_number, score };
+}
